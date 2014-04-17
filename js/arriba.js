1fbeac0f66029b1a21b9bdfcf4397f057fc9d959
Dennis Mairena <dmg19@hotmail.com>
2014-04-17 13:53:15 -0500

    v.31
    test botones

--- js/arriba.js
+++ js/arriba.js
@@ -2,19 +2,17 @@ jQuery.noConflict();
 jQuery(document).ready(function() {
 jQuery("#IrArriba").hide();
 jQuery("#volver").hide();
+jQuery("#tag").hide();
 jQuery(function () {
 jQuery(window).scroll(function () {
 if (jQuery(this).scrollTop() > 200) {
-jQuery('#IrArriba').fadeIn()
+jQuery('#IrArriba').fadeIn();
+jQuery('#volver').fadeOut();
+jQuery('#tag').fadeOut();
 } else {
-jQuery('#IrArriba').fadeOut()
-
-}
-if (jQuery(this).scrollTop() < 200) {
+jQuery('#IrArriba').fadeOut();
 ;jQuery('#volver').fadeIn();
-} else {
-jQuery('#volver').fadeOut()
-
+jQuery('#tag').fadeIn();
 }
 });
 jQuery('#IrArriba a').click(function () {
