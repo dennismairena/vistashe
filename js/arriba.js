ebac23b2f9db4e22b5984e8bee1d5d0e18b09799
Dennis Mairena <dmg19@hotmail.com>
2014-05-02 15:04:04 -0700

    v.1.30
    test

--- js/arriba.js
+++ js/arriba.js
@@ -6,7 +6,7 @@ jQuery("#volver").hide();
 jQuery(function () {
 jQuery(window).scroll(function () {
 if ($(this).scrollTop() > 200) {
-	jQuery('#panel3').fadeIn('slow');
+	jQuery('#panel2').fadeIn('slow');
 jQuery('#IrArriba').fadeIn('slow');
 jQuery('#volver').fadeIn('slow');
 jQuery('#tag').fadeOut('slow');jQuery('#IrArriba').fadeIn('slow');
@@ -18,7 +18,7 @@ jQuery('#com').fadeOut('slow');
 jQuery('#follow').fadeOut('slow');
 jQuery('#menu').fadeOut('slow');
 } else {
-	jQuery('#panel3').fadeOut('slow');
+	jQuery('#panel2').fadeOut('slow');
 jQuery('#IrArriba').fadeOut('slow');
 ;jQuery('#volver').fadeOut('slow');
 jQuery('#tag').fadeIn('slow');
@@ -49,8 +49,10 @@ scrollTop: 350
 }, 800);
 return false;
 });
-$( ".no,.share" ).click(function() {
-  jQuery('#panel3').fadeOut('slow');
+$( ".no,.share,#gatolike" ).click(function() {
+  jQuery('#panel2').fadeOut('slow');
+  jQuery('#panel3').fadeToggle('slow');
+
   return false;
 });
 jQuery('#follow').click(function () {
